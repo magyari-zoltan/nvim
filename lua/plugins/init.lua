179635ccc96472e6b5550f96f27670e5a5cd3a3e
Magyari Zoltán <github.config.emu037@passmail.com>
2024-08-23 16:47:47 +0300

    Paper colorscheme

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -24,8 +24,20 @@ local plugins = {
     lazy = false,    -- make sure we load this during startup if it is your main colorscheme
     priority = 1000, -- make sure to load this before all the other start plugins
     config = function()
+      executeCommand('set background=dark')
       executeCommand('colorscheme gruvbox')
     end
+  },
+
+  -- Paper colorscheme
+  {
+    'NLKNguyen/papercolor-theme',
+    lazy = false,    -- make sure we load this during startup if it is your main colorscheme
+    priority = 1000, -- make sure to load this before all the other start plugins
+    -- config = function()
+    --   executeCommand('set background=light')
+    --   executeCommand('colorscheme papercolor')
+    -- end
   },
 
   -- Notify
