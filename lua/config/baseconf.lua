f0671797339b13ef20800dd32203d1bb40131211
Magyari Zoltán <github.config.emu037@passmail.com>
2025-11-06 11:24:36 +0200

    Do not replace tabs with spaces

--- lua/config/baseconf.lua
+++ lua/config/baseconf.lua
@@ -1,8 +1,8 @@
 -- Tab
-vim.opt.tabstop = 4      -- number of visual spaces per TAB
-vim.opt.softtabstop = 4  -- number of spacesin tab when editing
-vim.opt.shiftwidth = 4   -- insert 4 spaces on a tab
-vim.opt.expandtab = true -- tabs are spaces, mainly because of python
+vim.opt.tabstop = 4       -- number of visual spaces per TAB
+vim.opt.softtabstop = 4   -- number of spacesin tab when editing
+vim.opt.shiftwidth = 4    -- insert 4 spaces on a tab
+vim.opt.expandtab = false -- tabs are spaces
 
 -- UI config
 vim.opt.number = true          -- show absolute number
