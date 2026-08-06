c8406322ed0b21419df0a7fc3aa85ec85d02c090
Magyari Zoltán <codesage.throttle642@passmail.net>
2026-08-06 21:42:19 +0300

    Terminal keybindings

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -26,17 +26,21 @@ vim.keymap.set('i', '<C-x>', '<ESC>:q!<Enter>', { noremap = true })
 vim.keymap.set('t', '<C-x>', '<C-\\><C-n>:q!<Enter>', { noremap = true })
 
 -- Window movements
--- vim.keymap.set('n', '<C-h>', '<C-w>h', { noremap = true })
--- vim.keymap.set('n', '<C-j>', '<C-w>j', { noremap = true })
--- vim.keymap.set('n', '<C-k>', '<C-w>k', { noremap = true })
--- vim.keymap.set('n', '<C-l>', '<C-w>l', { noremap = true })
--- vim.keymap.set('i', '<C-h>', '<C-w>h', { noremap = true })
--- vim.keymap.set('i', '<C-j>', '<C-w>j', { noremap = true })
--- vim.keymap.set('i', '<C-k>', '<C-w>k', { noremap = true })
--- vim.keymap.set('i', '<C-l>', '<C-w>l', { noremap = true })
+vim.keymap.set('n', '<M-h>', '<C-w>h', { noremap = true })
+vim.keymap.set('n', '<M-j>', '<C-w>j', { noremap = true })
+vim.keymap.set('n', '<M-k>', '<C-w>k', { noremap = true })
+vim.keymap.set('n', '<M-l>', '<C-w>l', { noremap = true })
+vim.keymap.set('i', '<M-h>', '<C-w>h', { noremap = true })
+vim.keymap.set('i', '<M-j>', '<C-w>j', { noremap = true })
+vim.keymap.set('i', '<M-k>', '<C-w>k', { noremap = true })
+vim.keymap.set('i', '<M-l>', '<C-w>l', { noremap = true })
 
 -- Source
 vim.keymap.set('n', '%%', ':source %<Enter>', { noremap = true })
+
+--------------------------------------------------------------------------------
+--- Terminal keybingins
+--------------------------------------------------------------------------------
 
 -- Terminal
 local function openTerminalWindow()
@@ -69,14 +73,24 @@ local function openClaudeTerminalWindow()
     executeCommand('startinsert')
 end
 
-vim.keymap.set('n', '<M-ö>', openTerminalWindow, { noremap = true })
-vim.keymap.set('n', '<M-ü>', openTerminalWindowRight, { noremap = true })
+-- Opening terminal
+vim.keymap.set('n', '<M-t>', openTerminalWindow, { noremap = true })
+vim.keymap.set('n', '<M-S-t>', openTerminalWindowRight, { noremap = true })
+
+-- Escaping terminl
+vim.keymap.set('t', '<Esc>', '<C-\\><C-n>', { noremap = true })
+vim.keymap.set('t', '<Esc><Esc>', '<Esc>', { noremap = true })
+
+-- Navigation inside a terminal
+vim.keymap.set('t', '<M-h>', '<C-\\><C-n><C-w>h', { noremap = true })
+vim.keymap.set('t', '<M-j>', '<C-\\><C-n><C-w>j', { noremap = true })
+vim.keymap.set('t', '<M-k>', '<C-\\><C-n><C-w>k', { noremap = true })
+vim.keymap.set('t', '<M-l>', '<C-\\><C-n><C-w>l', { noremap = true })
+
+--------------------------------------------------------------------------------
+--- Copilot keybingins
+--------------------------------------------------------------------------------
+
 vim.keymap.set('n', '<leader>cp', openCopilotTerminalWindow, { noremap = true })
 vim.keymap.set('n', '<leader>cd', openCodexTerminalWindow, { noremap = true })
 vim.keymap.set('n', '<leader>cl', openClaudeTerminalWindow, { noremap = true })
-vim.keymap.set('t', '<Esc>', '<C-\\><C-n>', { noremap = true })
-vim.keymap.set('t', '<Esc><Esc>', '<Esc>', { noremap = true })
-vim.keymap.set('t', '<C-w>h', '<C-\\><C-n><C-w>h', { noremap = true })
-vim.keymap.set('t', '<C-w>j', '<C-\\><C-n><C-w>j', { noremap = true })
-vim.keymap.set('t', '<C-w>k', '<C-\\><C-n><C-w>k', { noremap = true })
-vim.keymap.set('t', '<C-w>l', '<C-\\><C-n><C-w>l', { noremap = true })
